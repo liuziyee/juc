ee1b25ad031eaae622c8bdaf36fd1c3bf1199ed5
liuziyee <liuziyee1994@163.com>
2023-03-13 23:23:04 +0800

    :beers:

--- src/main/java/com/dorohedoro/thread/api/MockJoin.java
+++ src/main/java/com/dorohedoro/thread/api/MockJoin.java
@@ -1,26 +1,25 @@
 package com.dorohedoro.thread.api;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class JoinInterrupt {
+public class MockJoin {
 
+    @SneakyThrows
     public static void main(String[] args) {
         Thread main = Thread.currentThread();
         Thread son = new Thread(() -> {
             try {
                 Thread.sleep(500);
                 log.info("主线程状态: {}", main.getState());
-                main.interrupt();
                 Thread.sleep(3000);
                 log.info("子线程退出");
             } catch (InterruptedException e) {}
         });
         son.start();
-        try {
-            son.join();
-        } catch (InterruptedException e) {
-            log.info("主线程中断");
+        synchronized (son) {
+            son.wait();
         }
         log.info("主线程退出");
     }
